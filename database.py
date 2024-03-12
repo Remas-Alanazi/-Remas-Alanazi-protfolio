d932c13b2847de619818dd7196ab3dddac8cee9c
Remas-Alanazi <162414683+Remas-Alanazi@users.noreply.github.com>
2024-03-12 09:57:53 +0000

    connect with amazon RDS

---
+++ database.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine, text
+
+engine = create_engine(
+    "mysql+pymysql://admin:1234admin@database-1.cvksi6qkq589.eu-north-1.rds.amazonaws.com/projects?charset=utf8mb4"
+)
+with engine.connect() as conn:
+  result = conn.execute(text("select * from trial"))
+  print(result.all())
